bb355a7719ae719e0a89c68b5caaf905c2b8b18b
Daniele Esposito <daniele.esposito@webtrekk.com>
2020-11-10 20:13:35 +0100

    election

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-
-
-const btn = document.querySelector('#btn');
+const btn = document.querySelector("#btn");
 // cREATE GAME BOARD
 
 // Images Trump
-    let trumpImage = document.createElement("img");
-        trumpImage.setAttribute("src", "https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg")
-        trumpImage.classList.add("trumpstyle", "trump");
+let trumpImage = document.createElement("img");
+trumpImage.setAttribute(
+  "src",
+  "https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg"
+);
+trumpImage.classList.add("trumpstyle", "trump");
 
-        let singleLi = document.querySelectorAll(".trumpdiv ul li");
-    let listLength = document.querySelector(".trumpdiv ul").querySelectorAll("li").length;  
-    const trumpDiv =  document.querySelector(".trumpdiv");  
+let singleLi = document.querySelectorAll(".trumpdiv ul li");
+let listLength = document.querySelector(".trumpdiv ul").querySelectorAll("li")
+  .length;
+const trumpDiv = document.querySelector(".trumpdiv");
 
-function createImages () {
+function renderOne(i) { 
+  // 0 , 1, 2
+  console.log(i);
+  singleLi[i].appendChild(trumpImage)
+}
 
-    for (let i = 0; i < listLength; i++) {
-        console.log(singleLi[i])
-       singleLi[i].appendChild(trumpImage);  
-    }  
+function createImages() {
+  for (let i = 0; i < listLength; i++) {
+    console.log(singleLi[i]);
+    // singleLi[i].appendChild(trumpImage);
+    renderOne(i) // new scope -- function scope
   }
-  btn.addEventListener('click', () => {
-    createImages();
-  });
+}
 
-  
+btn.addEventListener("click", () => {
+  createImages();
+});
